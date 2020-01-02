a0e2505f43ad40525f18edfb6c1e1fae277a15c1
David Schoenholtz <dmschoenholtz@gmail.com>
2020-01-02 21:39:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,18 @@ class Student
   end
 end
 
+def self.create(name:, album:)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM songs WHERE name = ?"
+    result = DB[:conn].execute(sql, name)[0]
+    Song.new(result[0], result[1], result[2])
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
